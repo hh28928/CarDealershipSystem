a6ef8617aef168db98200c3cdaf18b14ee4776ad
hh28928 <43480980+hh28928@users.noreply.github.com>
2019-04-22 15:59:12 -0400

    Add files via upload

--- inventory/presenters/InventoryPresenter.java
+++ inventory/presenters/InventoryPresenter.java
@@ -1,21 +1,26 @@
 package inventory.presenters;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.util.Set;
 import java.util.HashSet;
-import mailing.brokers.MessageBroker;
-import mailing.publishers.Publisher;
-import mailing.messages.Message;
-import inventory.views.View;
-import inventory.views.MainView;
-import inventory.views.DetailView;
+
+import finance.models.FinanceCarModel;
+import inventory.models.CarModel;
+import inventory.models.InventoryCarModel;
 import inventory.views.InventoryCarRenderable;
-import inventory.models.*;
+import inventory.views.MainView;
+import inventory.views.View;
+import mailing.brokers.MessageBroker;
+import mailing.messages.Message;
+import mailing.publishers.Publisher;
 
 
 public class InventoryPresenter implements Publisher
 {
 	//the mailing list message broker
 	private MessageBroker broker;
+	String data_location = "CarDatabase.csv";
 	
 	//the currently-selected view, defaults to the main view
 	private View currentView;
@@ -36,7 +41,10 @@ public class InventoryPresenter implements Publisher
 		this.renderView();
 	}
 	
-	
+	public Set<InventoryCarModel> getInventoryItems() {
+		return inventoryItems;
+	}
+
 	//replace the current view with the specified view
 	public void switchView(View view) {
 		this.currentView = view;
@@ -69,17 +77,44 @@ public class InventoryPresenter implements Publisher
 	}
 	
 	
-	public void addInventoryItem() {
-		
+	public void addInventoryItem(String VIN, String make, String model, String color, int price) {
+		try{
+			CarModel addInventory = new CarModel(VIN, make, model, color);
+			InventoryCarModel inventoryCarModel = new InventoryCarModel(VIN, make, model, color, price);
+			FileWriter writer = new FileWriter(data_location,true);
+			BufferedWriter bufferedWriter = new BufferedWriter(writer);
+			bufferedWriter.write(VIN);
+			bufferedWriter.write(make);
+			bufferedWriter.write(model);
+			bufferedWriter.write(color);
+			bufferedWriter.close();
+			writer.close();
+			inventoryItems.add(inventoryCarModel);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Sorry, but something was corupted with the information givin to addFinancedCar");
+		}
 	}
 	
 	
-	public void updateInventoryItem() {
-		
+	public void updateInventoryItem(InventoryCarModel update_car) {
+		for (InventoryCarModel s : inventoryItems) {
+			if (s.getVIN().equals(update_car.getVIN())) {
+				deleteInventoryItem(s);
+				addInventoryItem(update_car.getVIN(), update_car.getMake(), update_car.getModel(), update_car.getColor(), update_car.getPrice());
+			}
+		}
 	}
 	
 	
-	public void deleteInventoryItem() {
-		
+	public void deleteInventoryItem(InventoryCarModel removeCar) {
+		try{
+			this.inventoryItems.remove(removeCar);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Sorry, but something was corupted with the information givin to remoFinancedCar");
+		}
 	}
 }
