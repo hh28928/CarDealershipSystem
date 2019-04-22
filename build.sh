f5a7ff94b478d5950e27b3cf5b2989682170fa52
Ryan Hoang <rhoang@gmu.edu>
2019-04-21 20:22:43 -0400

    Dependency issues resolved. Readding intellij to gitignore.

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-export CLASSPATH=$CLASSPATH:"$PWD/forms_rt-7.0.3.jar" ;./clean.sh;find -name "*.java" > sources.txt; javac @sources.txt; rm sources.txt;
+export CLASSPATH=$CLASSPATH:"$PWD/forms_rt-7.0.3.jar" ; export CLASSPATH=$CLASSPATH:"$PWD/jgoodies-forms-1.6.0.jar"; export CLASSPATH=$CLASSPATH:"$PWD/swingx-1.6.1.jar";./clean.sh;find -name "*.java" > sources.txt; javac @sources.txt; rm sources.txt;
